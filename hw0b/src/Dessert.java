e5d2a65feeb95eba77ba2b390304a43fd09246e0
NarezIn <zn2133@nyu.edu>
2025-05-21 11:24:07 +0800

    Finished HW0B Dessert

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,2 +1,21 @@
+import java.awt.desktop.SystemSleepEvent;
+
 public class Dessert {
+    private int flavor;
+    private int price;
+    public static int total = 0;
+
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        total++;
+    }
+
+    public void printDessert(){
+        System.out.println(flavor + " " + price + " " + total);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I love dessert!");
+    }
 }
