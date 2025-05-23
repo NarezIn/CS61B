c114c9681f7cca04c5ff2454a5238363f28326d3
NarezIn <zn2133@nyu.edu>
2025-05-23 15:52:06 +0800

    Finished Proj_hardmode

--- proj0_hardmode/src/game2048logic/GameLogic.java
+++ proj0_hardmode/src/game2048logic/GameLogic.java
@@ -6,6 +6,8 @@ import static game2048logic.MatrixUtils.rotateRight;
 
 /**
  * @author  Josh Hug
+ * Filled in by NarezIn:
+ * I peeked at the instruction of the normal version to get some ideas.
  */
 public class GameLogic {
     /**
@@ -18,17 +20,89 @@ public class GameLogic {
     public static void tilt(int[][] board, Side side) {
         // fill this in
         if (side == Side.NORTH) {
-            // Don't you dare try to write all of your
-            // code in this method. You will want to write
-            // helper methods. And those helper methods should
-            // have helper methods.
-            return;
+            tiltUp(board);
         } else if (side == Side.EAST) {
-            return;
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
         } else if (side == Side.WEST) {
-            return;
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
         } else { // SOUTH
-            return;
+            rotateRight(board);
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
+            rotateLeft(board);
+        }
+    }
+
+    /**
+     * Given the index of the column,
+     * moving tiles in that column all the way up
+     *
+     * @param board the current state of the board
+     * @param iCol the destining index determined by the direction
+     *
+     */
+    public static void moveUpCol(int[][] board, int iCol) {
+        for (int ii = 0; ii < board.length; ii++) {
+            if (board[ii][iCol] != 0) {
+                for (int i = ii; i > 0; i--) {
+                    if (board[i - 1][iCol] == 0) {
+                        board[i - 1][iCol] = board[i][iCol];
+                        board[i][iCol] = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Given the board that has been moved up and the index of the column,
+     * merging tiles in that column all the way up
+     *
+     * @param board the current state of the board
+     * @param iCol the destining index determined by the direction
+     *
+     */
+    public static void mergeUpCol(int[][] board, int iCol) {
+        int numMerge = 0;
+        for (int i = 0; i < board.length - 1; i++) {
+            if (board[i][iCol] != 0 && board[i][iCol] == board[i + 1][iCol]) {
+                numMerge++;
+                i++;
+            }
+        }
+        int kk = 0;
+        while (numMerge > 0 && kk < board.length){
+            if (board[kk][iCol] == board[kk + 1][iCol]) {
+                board[kk][iCol] = board[kk][iCol] + board[kk][iCol];
+                int jj = 1;
+                while ((kk + jj) < board.length - 1) {
+                    board[kk + jj][iCol] = board[kk + jj + 1][iCol];
+                    jj++;
+                }
+                board[kk+jj][iCol] = 0;
+                numMerge--;
+            }
+            else if (board[kk][iCol] == 0) {
+                break;
+            }
+            kk++;
+        }
+    }
+
+    /**
+     * Traversing through every column to move every tile up.
+     *
+     * @param board the current state of the board
+     */
+    public static void tiltUp(int[][] board) {
+        for (int ii = 0; ii < board.length; ii++) {
+            moveUpCol(board, ii);
+            mergeUpCol(board, ii);
         }
     }
 }
