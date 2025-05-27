7f9b6b98feb991094f6943862af2375ee9a4e38d
NarezIn <zn2133@nyu.edu>
2025-05-27 22:34:19 +0800

    Finishing Proj1A addFirst addLast

---
+++ proj1a/src/LinkedListDeque61B.java
@@ -0,0 +1,89 @@
+import java.util.List;
+
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    private Node sentinel;
+    private int size;
+
+    /**
+     * Node the nested class
+     */
+    public class Node{
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(){
+            item = null;
+            prev = null;
+            next = null;
+        }
+
+        public Node(T i, Node p, Node n){
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    /**
+     * The empty constructor
+     */
+    public LinkedListDeque61B() {
+        sentinel = new Node();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+
+        size = 0;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        Node newNode = new Node(x, sentinel, sentinel.next);
+        sentinel.next.prev = newNode;
+        sentinel.next = newNode;
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        Node newNode = new Node(x, sentinel.prev, sentinel);
+        sentinel.prev.next = newNode;
+        sentinel.prev = newNode;
+        size += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
